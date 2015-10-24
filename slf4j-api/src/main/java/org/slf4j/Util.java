74a94c670288aa33f68fc685edc1731343cfac1e
Nathan Green <nathansgreen@users.noreply.github.com>
2015-10-23 20:48:15 -0500

    Create "safe" supplier get() methods and use them as the default for Logger.

--- slf4j-api/src/main/java/org/slf4j/Util.java
+++ slf4j-api/src/main/java/org/slf4j/Util.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2015 QOS.ch
  * All rights reserved.
- *
+ * <p/>
  * Permission is hereby granted, free  of charge, to any person obtaining
  * a  copy  of this  software  and  associated  documentation files  (the
  * "Software"), to  deal in  the Software without  restriction, including
@@ -9,10 +9,10 @@
  * distribute,  sublicense, and/or sell  copies of  the Software,  and to
  * permit persons to whom the Software  is furnished to do so, subject to
  * the following conditions:
- *
+ * <p/>
  * The  above  copyright  notice  and  this permission  notice  shall  be
  * included in all copies or substantial portions of the Software.
- *
+ * <p/>
  * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
  * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
  * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
@@ -20,7 +20,6 @@
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
  * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 package org.slf4j;
 
@@ -30,12 +29,46 @@ package org.slf4j;
 class Util {
     private Util() {}
 
+    private static final Object[] EMPTY_ARRAY = new Object[0];
+
     static Object get(Supplier<?> arg) {
-        return arg == null ? null : arg.get();
+        return safeGet(arg);
+    }
+
+    static Object[] getAll(Supplier<?>[] arguments) {
+        return safeGetAll(arguments);
     }
 
     static Object[] get(ArraySupplier<?> arg) {
-        return arg == null ? new Object[0] : arg.get();
+        return safeGet(arg);
+    }
+
+    static Object unsafeGet(Supplier<?> arg) {
+        return arg == null ? null : arg.get();
+    }
+
+    static Object safeGet(Supplier<?> arg) {
+        try {
+            return arg == null ? null : arg.get();
+        } catch (Throwable t) {
+            System.err.println("SLF4J: Failed invocation of [Supplier<?>.get()]");
+            t.printStackTrace();
+            return null;
+        }
+    }
+
+    static Object[] unsafeGet(ArraySupplier<?> arg) {
+        return arg == null ? EMPTY_ARRAY : arg.get();
+    }
+
+    static Object[] safeGet(ArraySupplier<?> arg) {
+        try {
+            return arg == null ? EMPTY_ARRAY : arg.get();
+        } catch (Throwable t) {
+            System.err.println("SLF4J: Failed invocation of [ArraySupplier<?>.get()]");
+            t.printStackTrace();
+            return null;
+        }
     }
 
     /**
@@ -44,13 +77,24 @@ class Util {
      * @param arguments one or more suppliers
      * @return results of evaluating all suppliers
      */
-    static Object[] getAll(Supplier<?>[] arguments) {
-        if (arguments == null) {
-            return new Object[0];
+    static Object[] unsafeGetAll(Supplier<?>[] arguments) {
+        if(arguments == null || arguments.length < 1) {
+            return EMPTY_ARRAY;
         }
         Object[] args = new Object[arguments.length];
-        for (int i = 0; i < arguments.length; i++) {
-            args[i] = get(arguments[i]);
+        for(int i = 0; i < arguments.length; i++) {
+            args[i] = unsafeGet(arguments[i]);
+        }
+        return args;
+    }
+
+    static Object[] safeGetAll(Supplier<?>[] arguments) {
+        if(arguments == null || arguments.length < 1) {
+            return EMPTY_ARRAY;
+        }
+        Object[] args = new Object[arguments.length];
+        for(int i = 0; i < arguments.length; i++) {
+            args[i] = safeGet(arguments[i]);
         }
         return args;
     }
